239339eae698f87b7b0cb6d5fb25716f28859293
“zuzia219 <zuzia219@mail.com>
2017-12-07 10:42:33 +0100

    second commit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,7 @@
+package com.kodilla.testing;
+
+public class TestingMain {
+    public static void main(String[] args) {
+        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    }
+}
