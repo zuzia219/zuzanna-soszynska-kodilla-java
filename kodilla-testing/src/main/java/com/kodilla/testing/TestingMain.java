12969e0f81441b2e84db83a77599647bd90b868e
“zuzia219 <zuzia219@mail.com>
2017-12-07 11:23:17 +0100

    Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
+
 public class TestingMain {
-    public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args){
+        Calculator calculator = new Calculator();
+
+         // test1
+        int result1 = calculator.add(5,4);
+
+        if (result1 == 9)
+            System.out.println("test 1 OK");
+         else
+            System.out.println("Error!");
+
+
+        // test2
+        int result2 = calculator.substract(5,4);
+
+        if (result2 == 1)
+            System.out.println("test 2 OK");
+        else
+            System.out.println("Error!");
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/com/kodilla/testing/user/SimpleUser.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.com.kodilla.testing.user;
+
+public class SimpleUser {
+
+    private String username;
+
+    public SimpleUser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
