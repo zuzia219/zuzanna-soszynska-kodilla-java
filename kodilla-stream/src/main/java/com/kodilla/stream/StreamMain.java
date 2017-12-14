d81681683469d671177fba3d7e9d6ff419bfd5ce
“zuzia219 <zuzia219@mail.com>
2017-12-14 11:32:00 +0100

    Task 7.2 immutable class ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -13,5 +13,8 @@ public class StreamMain {
         poemBeautifier.beautify("", "PoemTitle", ((decoration, toBeDecorated) -> decoration + toBeDecorated.toUpperCase()));
         poemBeautifier.beautify("***", "PoemTitle", ((decoration, toBeDecorated) -> decoration + toBeDecorated.toLowerCase() + decoration));
         poemBeautifier.beautify("&", "PoemTitle", ((decoration, toBeDecorated) -> decoration + toBeDecorated.replace('T', 't') + decoration));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
